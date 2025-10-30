9d1545e408c06bd2f42ec7765ad319ad4ac8b0ee
Hansol Jeong <hansol.jeong@flitto.com>
2025-10-30 11:35:31 +0900

    feat: DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true, // dev only
+      logging: true, // SQL 쿼리 로그 출력
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
