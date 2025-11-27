3e9c1e1822ea7ce99d4ed0c167b2be2c7e272d08
Hansol Jeong <hansol.jeong@flitto.com>
2025-11-27 17:24:36 +0900

    build: 로컬 DB 설정을 위한 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,8 +19,9 @@ import { UserSettingModule } from './modules/user-settings/user-setting.module';
       port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      database: process.env.DB_NAME,
       entities: [UserGroup, User, UserSetting],
+      synchronize: true, // Auto-create database schema. Set to false in production.
     }),
     UserGroupModule,
     UserModule,
