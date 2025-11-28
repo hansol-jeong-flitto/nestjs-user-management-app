97bcb5afd89ee825cb127255f622bbdc85b8da7d
Hansol Jeong <hansol.jeong@flitto.com>
2025-11-28 12:49:21 +0900

    feat: Validation Pipe 고도화 / 예외 필터 적용 / interceptor 적용

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionsFilter } from './shared/filters/http-exception.filter'; // Import the filter
+import { TransformInterceptor } from './shared/interceptors/transform.interceptor'; // Import the interceptor
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe()); // 옵션 설정은 필요에 따라 추가
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, // Automatically transform payloads to DTO instances
+    }),
+  );
+
+  app.useGlobalFilters(new AllExceptionsFilter()); // Apply the global exception filter
+  app.useGlobalInterceptors(new TransformInterceptor()); // Apply the global response interceptor
 
   const config = new DocumentBuilder()
     .setTitle('User Management API')
